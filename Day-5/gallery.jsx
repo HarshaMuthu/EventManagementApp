28fa0be23287e6e084136e93586c3da7ae664886
HarshaMuthu <113664562+HarshaMuthu@users.noreply.github.com>
2023-07-28 11:50:51 +0530

    Add files via upload

---
+++ Day-5/gallery.jsx
@@ -0,0 +1,39 @@
+import { useSelector } from "react-redux";
+import { Image } from "./image";
+import React from "react";
+import Typography from "typography";
+
+export const Gallery = (props) => {
+  
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          
+          <p>
+            Immerse Yourself in Spectacular Moments...
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
